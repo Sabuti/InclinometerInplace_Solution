2b3eea91dc562887faf7245c97d507e684cae43a
Lucas Linhares <lucasa.linhares1@gmail.com>
2025-06-30 17:31:12 -0300

    teste codigo basico mestre/escravo

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -1,8 +1,23 @@
 #include <Arduino.h>
+#include <ModbusRTUSlave.h>
+#include <SoftwareSerial.h>
+
+#define RX 2
+#define TX 3
+#define RTS 4
+
+SoftwareSerial rs485Serial(RX, TX);
+ModbusRTUSlave slave;
+
+uint16_t registers[10] = {1234};  // registrador 0 vai conter 1234
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Nano pronto!");
+  pinMode(RTS, OUTPUT);
+  rs485Serial.begin(9600);
+
+  slave.begin(1, 9600);  // ID 1, usa RTS para RE/DE
+
+  slave.configureHoldingRegisters(registers, sizeof(registers) / sizeof(registers[0]));
 }
 
 void loop() {
--- src/esp32/main.cpp
+++ src/esp32/main.cpp
@@ -1,10 +1,33 @@
 #include <Arduino.h>
+#include <ModbusRTUMaster.h>
+
+#define TXD 15
+#define RXD 16
+#define RTS 17
+
+HardwareSerial RS485Serial(2);
+ModbusRTUMaster master;
+
+uint16_t response[10];  // buffer para armazenar valores lidos
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("ESP32 online");
+  RS485Serial.begin(9600, SERIAL_8N1, RXD, TXD);
+  master.begin(RS485Serial, RTS);
 }
 
 void loop() {
-  // nada aqui
+  static uint32_t last = 0;
+
+  if (millis() - last > 1000) {
+    last = millis();
+
+    // Lê 1 registrador do escravo ID 1, endereço 0
+    if (master.readHoldingRegisters(1, 0, response, 1)) {
+      Serial.print("Valor lido do escravo: ");
+      Serial.println(response[0]);
+    } else {
+      Serial.println("Falha na leitura.");
+    }
+  }
 }
